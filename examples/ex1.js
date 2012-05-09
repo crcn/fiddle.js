d6e799d1df211f32ec2e756d69d57061adc46c93
Craig Condon <craig@spiceapps.com>
2012-05-09 14:45:05 -0500

    return true / false if object has been modified

--- examples/ex1.js
+++ examples/ex1.js
@@ -35,9 +35,11 @@ var targets = [
 
 var fiddler1 = fiddle({$inc:{age:1}}),
 fiddler2     = fiddle({$set:{"comments.$.date": Date.now(), "comments.$.replies.$.date": Date.now() }}),
-fiddler3     = fiddle({$addToSet:{hobbies:'cooking'}});
+fiddler3     = fiddle({$addToSet:{hobbies:'cooking'}}),
+fiddler4     = fiddle({$rename:{doesNotExist:'cooking'}});
 
 
-fiddler2(targets);
-fiddler3(targets);
+console.log(fiddler2(targets));
+console.log(fiddler3(targets));
+console.log(fiddler4(targets));
 console.log(JSON.stringify(targets, null, 2));
